9764bd3c2d23a18e5ba285a22802583cd5202f6f
tewa <no@nope>
2023-03-05 16:19:23 +0100

    stuff

--- commands.mjs
+++ commands.mjs
@@ -59,7 +59,7 @@ const info = (name) => {
   const thing = global.players[name];
 
   if (!thing) {
-    return `\`\`\`No data on ${name} right now. Try in a few ticks.\`\`\``;
+    return `No data on ${name} right now. Try in a few ticks.`;
   } else {
     let returner = '```';
 
@@ -78,47 +78,128 @@ const info = (name) => {
   }
 };
 
+const hp = (name) => {
+  const player = global.players[name];
+  if (player) {
+    if (player.health.health >= 90) {
+      return `${name} looks healthy to me.`;
+    } else if (player.health.health < 90 && player.health.health >= 50) {
+      return `${name} should probably take care of those wounds.`;
+    } else if (player.health.health < 50 && player.health.health >= 20) {
+      return `${name} is pretty beat up.`;
+    } else if (player.health.health < 20 && player.health.health >= 10) {
+      return `Uh, can someone help ${name}?`;
+    } else if (player.health.health < 10 && player.health.health >= 1) {
+      return `How ${name} is still alive is a mystery to me.`;
+    } else {
+      return `${name} has a serious case of deadness.`;
+    }
+  } else {
+    return `No data on ${name} right now. Try in a few ticks.`;
+  }
+};
+
+const infected = (name) => {
+  const player = global.players[name];
+  if (player) {
+    return `${name} ${player.health.infected ? 'is' : 'is not'} infected.`;
+  } else {
+    return `No data on ${name} right now. Try in a few ticks.`;
+  }
+};
+
+const profession = (name) => {
+  const player = global.players[name];
+  if (player) {
+    return `${name} is a ${player.stats.profession}.`;
+  } else {
+    return `No data on ${name} right now. Try in a few ticks.`;
+  }
+};
+
+const hours = (name) => {
+  const player = global.players[name];
+  if (player) {
+    return `${name} has been alive for ${player.stats.hours} hours.`;
+  } else {
+    return `No data on ${name} right now. Try in a few ticks.`;
+  }
+};
+
 const parse = async (msg, sender) => {
   const words = msg.split(' ');
   const command = words.shift();
 
-  const pretext = `(${sender})[!${command}${words.length ? ' ' + words.join(' ') : ''}] `;
+  const pretext = `🖨️\`(${sender})[!${command}${words.length ? ' ' + words.join(' ') : ''}]\` `;
 
   try {
     switch (command) {
       case 'players': {
-        return `\`\`\`${pretext}${await players()}\`\`\``;
+        return `${pretext}${await players()}`;
       }
 
       case 'info': {
         if (words.length >= 1) {
           return info(words.join(' '));
         } else {
-          return `\`\`\`${pretext}Gotta need a name on the dude, yo.\`\`\``;
+          return `${pretext}Gotta need a name on the dude, yo.`;
         }
       }
 
       case 'stats': {
         if (words.length >= 1) {
-          return `\`\`\`${pretext}${stats(words.join(' '))}\`\`\``;
+          return `${pretext}${stats(words.join(' '))}`;
         } else {
-          return `\`\`\`${pretext}Gotta need a name on the dude, yo.\`\`\``;
+          return `${pretext}Gotta need a name on the dude, yo.`;
         }
       }
 
       case 'health': {
         if (words.length >= 1) {
-          return `\`\`\`${pretext}${health(words.join(' '))}\`\`\``;
+          return `${pretext}${health(words.join(' '))}`;
         } else {
-          return `\`\`\`${pretext}Gotta need a name on the dude, yo.\`\`\``;
+          return `${pretext}Gotta need a name on the dude, yo.`;
         }
       }
 
       case 'perks': {
         if (words.length >= 1) {
-          return `\`\`\`${pretext}${perks(words.join(' '))}\`\`\``;
+          return `${pretext}${perks(words.join(' '))}`;
         } else {
-          return `\`\`\`${pretext}Gotta need a name on the dude, yo.\`\`\``;
+          return `${pretext}Gotta need a name on the dude, yo.`;
+        }
+      }
+
+      case 'hp': {
+        if (words.length >= 1) {
+          return `${pretext}${hp(words.join(' '))}`;
+        } else {
+          return `${pretext}Gotta need a name on the dude, yo.`;
+        }
+      }
+
+      case 'infected': {
+        if (words.length >= 1) {
+          return `${pretext}${infected(words.join(' '))}`;
+        } else {
+          return `${pretext}Gotta need a name on the dude, yo.`;
+        }
+      }
+
+        
+      case 'profession': {
+        if (words.length >= 1) {
+          return `${pretext}${profession(words.join(' '))}`;
+        } else {
+          return `${pretext}Gotta need a name on the dude, yo.`;
+        }
+      }
+
+      case 'hours': {
+        if (words.length >= 1) {
+          return `${pretext}${hours(words.join(' '))}`;
+        } else {
+          return `${pretext}Gotta need a name on the dude, yo.`;
         }
       }
 
@@ -126,13 +207,13 @@ const parse = async (msg, sender) => {
         if (words.length < 1) {
           const persistent = global.players?.persistent;
             return Number.isInteger(persistent?.totalKills) ?
-              `\`\`\`${pretext}Total kills for all players is ${persistent.totalKills}.\`\`\`` :
-              `\`\`\`${pretext}No total kills data right now.\`\`\``;
+              `${pretext}Total kills for all players is ${persistent.totalKills}.` :
+              `${pretext}No total kills data right now.`;
         } else {
           const persistentPlayer = global.players?.persistent[words.join(' ')];
           return Number.isInteger(persistentPlayer?.totalKills) ?
-            `\`\`\`${pretext}Total kills for ${words.join(' ')} is ${persistentPlayer.totalKills}.\`\`\`` :
-            `\`\`\`${pretext}No total kills data for ${words.join(' ')}.\`\`\``;
+            `${pretext}Total kills for ${words.join(' ')} is ${persistentPlayer.totalKills}.` :
+            `${pretext}No total kills data for ${words.join(' ')}.`;
         }
       }
 
@@ -140,13 +221,13 @@ const parse = async (msg, sender) => {
         if (words.length < 1) {
           const persistent = global.players?.persistent;
             return Number.isInteger(persistent?.totalDeaths) ?
-              `\`\`\`${pretext}Total deaths for all players is ${persistent.totalDeaths}.\`\`\`` :
-              `\`\`\`${pretext}No total deaths data right now.\`\`\``;
+              `${pretext}Total deaths for all players is ${persistent.totalDeaths}.` :
+              `${pretext}No total deaths data right now.`;
         } else {
           const persistentPlayer = global.players?.persistent[words.join(' ')];
           return Number.isInteger(persistentPlayer?.totalDeaths) ?
-            `\`\`\`${pretext}Total deaths for ${words.join(' ')} is ${persistentPlayer.totalDeaths}.\`\`\`` :
-            `\`\`\`${pretext}No total deaths data for ${words.join(' ')}.\`\`\``;
+            `${pretext}Total deaths for ${words.join(' ')} is ${persistentPlayer.totalDeaths}.` :
+            `${pretext}No total deaths data for ${words.join(' ')}.`;
         }
       }
 
@@ -166,30 +247,34 @@ const parse = async (msg, sender) => {
           if (thing) {
             return `${pretext}https://map.projectzomboid.com/#${thing.coords}`;
           } else {
-            return `\`\`\`${pretext}No data on ${name} right now. Try in a few ticks.\`\`\``;
+            return `${pretext}No data on ${name} right now. Try in a few ticks.`;
           }
         } else {
-          return `\`\`\`${pretext}Gotta need a name on the dude, yo.\`\`\``;
+          return `${pretext}Gotta need a name on the dude, yo.`;
         }
       }
 
       case 'servermsg': case 'send': {
         if (words.length < 1) {
-          return `\`\`\`${pretext}Can\'t just say nothing, boss. Give me a message to send.\`\`\``;
+          return `${pretext}Can\'t just say nothing, boss. Give me a message to send.`;
         }
 
         const msg = await servermsg(sender, words.join(' '));
-        return `\`\`\`${msg}\`\`\``;
+        return `${msg}`;
       }
 
       case 'json': {
         const string = JSON.stringify({ players: global.players });
-        return `\`\`\`${string}\`\`\``
+        return `${string}`
       }
 
       case 'help': {
         const { prefix } = global.config;
-        return `\`\`\`${pretext}
+        return `${pretext}\`\`\`
+${prefix}hp Ted - check Ted's condition.
+${prefix}infected Ted - check if Ted is infected.
+${prefix}profession Ted - check what Ted works with.
+${prefix}hours Ted - check how long Ted has been alive.
 ${prefix}info Ted - full info on Ted
 ${prefix}stats Ted - stats parts on Ted
 ${prefix}health Ted - Ted's health
